42572a8cccbd05090f47f6faff1e839a37bd4cc1
Damian Rozpedowski <rozpedowski.damian@gmail.com>
2024-02-27 20:53:52 -0500

    added complete project 1 and base project 2 files

---
+++ project1/search/search.py
@@ -0,0 +1,249 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+
+def depthFirstSearch(problem: SearchProblem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print("Start:", problem.getStartState())
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+    """
+    "*** YOUR CODE HERE ***"
+    # initialization
+    visited = []
+    actions = []
+    stack = util.Stack()
+    # push start state and empty actions
+    stack.push((problem.getStartState(),actions))
+    
+    while(True):
+        # if no candidates return failure
+        if stack.isEmpty(): 
+            return 0
+        
+        leafNode = stack.pop()
+        state = leafNode[0]
+        actions = leafNode[1]
+
+        # if the node contains goal state then return the corresponding solution
+        if problem.isGoalState(state):
+            return actions
+        
+        # expand non visited states
+        if state not in visited:
+            visited.append(state)
+            successors = problem.getSuccessors(state)
+            # iterate successors
+            for successor in successors:
+                nextState = successor[0]
+                nextAction = successor[1]
+
+                if nextState not in visited:
+                    # actions is a list, nextAction is a string, they must be of the same type when concatenating
+                    stack.push((nextState, actions + [nextAction]))
+    util.raiseNotDefined()
+
+def breadthFirstSearch(problem: SearchProblem):
+    """Search the shallowest nodes in the search tree first."""
+    "*** YOUR CODE HERE ***"
+    ## Queue
+    # initialization
+    visited = []
+    actions = []
+    queue = util.Queue()
+    # push start state and empty actions
+    queue.push((problem.getStartState(),actions,0))
+    
+    while(True):
+        # if no candidates return failure
+        if queue.isEmpty(): 
+            return 0
+        
+        leafNode = queue.pop()
+        state = leafNode[0]
+        actions = leafNode[1]
+        # if the node contains goal state then return the corresponding solution
+        if problem.isGoalState(state):
+            return actions
+        
+        # expand non visited states
+        if state not in visited:
+            visited.append(state)
+            successors = problem.getSuccessors(state)
+            # iterate successors
+            for successor in successors:
+                nextState = successor[0]
+                nextAction = successor[1]
+                if nextState not in visited:
+                    # actions is a list, nextAction is a string, they must be of the same type when concatenating
+                    queue.push((nextState, actions + [nextAction], 0))
+    util.raiseNotDefined()
+
+def uniformCostSearch(problem: SearchProblem):
+    """Search the node of least total cost first."""
+    "*** YOUR CODE HERE ***"
+    # Priority Queue
+    visited = []
+    actions = []
+    pqueue = util.PriorityQueue()
+    # push start state and empty actions
+    pqueue.push((problem.getStartState(),actions),0)
+    
+    while(True):
+        # if no candidates return failure
+        if pqueue.isEmpty(): 
+            return 0
+        
+        leafNode = pqueue.pop()
+        state = leafNode[0]
+        actions = leafNode[1]
+
+        # if the node contains goal state then return the corresponding solution
+        if problem.isGoalState(state):
+            return actions
+        
+        # expand non visited states
+        if state not in visited:
+            visited.append(state)
+            successors = problem.getSuccessors(state)
+            # iterate successors
+            for successor in successors:
+                nextState = successor[0]
+                nextAction = successor[1]
+                
+                if nextState not in visited:
+                    totalCost = problem.getCostOfActions(actions + [nextAction])
+                    # actions is a list, nextAction is a string, they must be of the same type when concatenating
+                    pqueue.push((nextState, actions + [nextAction]),totalCost)
+    util.raiseNotDefined()
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    # Priority Queue and heuristic
+    visited = []
+    actions = []
+    pqueue = util.PriorityQueue()
+    startState = problem.getStartState()
+    # push start state and empty actions
+    pqueue.push((startState,actions), 0 + heuristic(startState, problem))
+    
+    while(True):
+        # if no candidates return failure
+        if pqueue.isEmpty(): 
+            return 0
+        
+        leafNode = pqueue.pop()
+        state = leafNode[0]
+        actions = leafNode[1]
+
+        # if the node contains goal state then return the corresponding solution
+        if problem.isGoalState(state):
+            return actions
+        
+        # expand non visited states
+        if state not in visited:
+            visited.append(state)
+            successors = problem.getSuccessors(state)
+            # iterate successors
+            for successor in successors:
+                nextState = successor[0]
+                nextAction = successor[1]
+                
+                if nextState not in visited:
+                    totalCost = problem.getCostOfActions(actions + [nextAction])
+                    # actions is a list, nextAction is a string, they must be of the same type when concatenating
+                    pqueue.push((nextState, actions + [nextAction]),totalCost + heuristic(nextState, problem))
+    util.raiseNotDefined()
+
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
